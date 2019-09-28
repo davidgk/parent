f8771f34b0593532717c016085a84e0176236321
David Kotlirevsky <david.kotlirevsky@gmail.com>
2019-09-28 10:22:21 -0300

    First Commmit About migrate this app to react

--- frontend/src/main/frontend/src/app/router/app.routing.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from "@angular/router";
-import {PingComponent} from "../components/ping/ping.component";
-import {CleanScreenComponent} from "../components/clean-screen/clean-screen.component";
-
-
-export const appRoutes: Routes = [
-  { path: 'ping', component: PingComponent},
-  { path: 'clean-screen', component: CleanScreenComponent}
-];
